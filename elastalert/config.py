2f2d27c2725ce4ff16ce3eed0226b67841c90c24
Oliver Zampieri <oliver.zampieri@gmail.com>
2017-08-23 12:46:27 +0100

    Implement pulling rules from s3

--- elastalert/config.py
+++ elastalert/config.py
@@ -125,6 +125,19 @@ def load_configuration(filename, conf, args=None, s3=False):
     load_modules(rule, args)
     return rule
 
+def load_s3_rule_yaml(filename, conf):
+    logging.debug("Load rule from s3" + filename)
+    s3client = boto3.client("s3")
+    s3_object = s3client.get_object( 
+                Bucket=conf["s3_bucket"],
+                Key=filename
+                )
+    rule = yaml.load(s3_object["Body"])
+    rule.update({
+        'rule_file': filename,
+        'storage_type': 's3'
+    })
+    return rule
 
 def load_s3_rule_yaml(filename, conf):
     logging.debug("Load rule from s3" + filename)
@@ -564,17 +577,34 @@ def adjust_deprecated_values(rule):
         logging.warning('"simple" alerter has been renamed "post" and comptability may be removed in a future release.')
 
 
+<<<<<<< HEAD
 def list_s3_objects(conf):
     objects = []
     if conf.get("s3_bucket", False):
+=======
+
+def list_s3_objects(conf):
+    objects = []
+    if conf.get("s3_bucket", False):
+        s3 = boto3.resource('s3')
+>>>>>>> Implement pulling rules from s3
         s3client = boto3.client('s3')
         try:
             all_objects = s3client.list_objects_v2(
                     Bucket=conf["s3_bucket"],
                     Prefix=conf["s3_prefix"])
             for obj in all_objects["Contents"]:
+<<<<<<< HEAD
                 if obj["Key"].endswith(".yaml"):
                     objects.append(obj)
         except Exception as e:
             logging.warning("failed to access S3")
     return objects
+=======
+                if obj["Key"].endswith(".yaml"): objects.append(obj)
+        except Exception as e:
+            logging.exception("failed to access S3")
+    return objects
+
+
+>>>>>>> Implement pulling rules from s3
